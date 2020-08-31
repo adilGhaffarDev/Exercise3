0e74a6a8b0c1dfb90e851b0b5f1ead32c1f3f4a7
adil ghaffar <adilghaffar64@gmail.com>
2020-09-01 00:20:23 +0300

    uibuild

---
+++ build/precache-manifest.d03e9a13f2834bc6af8f73e7080e645a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0ccb5bc964dab011e00dfa6ba1ad93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32cffb9535925bb0cfa0",
+    "url": "/static/css/main.8752d791.chunk.css"
+  },
+  {
+    "revision": "0e0dc3953e29ded50ab8",
+    "url": "/static/js/2.7e9e8649.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e9e8649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cffb9535925bb0cfa0",
+    "url": "/static/js/main.b2014ba6.chunk.js"
+  },
+  {
+    "revision": "a2f539053e0e52506c79",
+    "url": "/static/js/runtime-main.0cfeee09.js"
+  }
+]);
