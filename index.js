27289fe755b9571c94c873f4f9627cc7def3e77a
adil ghaffar <adilghaffar64@gmail.com>
2020-09-01 00:02:42 +0300

    heroku server changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function getId (req) {
     return JSON.stringify(req.body)
@@ -32,6 +34,10 @@ let persons = [
       "id": 4
     }
   ]
+
+app.get('/',(request,response) => {
+    response.json(persons)
+})
 
 //get all persons data
 app.get('/api/persons',(request,response) => {
@@ -103,7 +109,7 @@ app.post('/api/persons',(request,response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
